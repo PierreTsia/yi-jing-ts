a0d7d348ac0b37dabbf6ebfb9514c278589ee66a
PierreTsia <pierre.tsiakkaros@gmail.com>
2022-02-04 18:34:39 +0100

    refactor(ui): :lipstick: change for one input more user friendly

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,32 +1,60 @@
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
+import { ref, defineComponent, computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import { useUserStore } from '~/stores/user'
 import Hexagram from '~/components/Hexagram.vue'
-import CoinsInput from '~/components/CoinsInput.vue'
 import { useHexagrams } from '~/composables/hexagrams'
+
+type Args = { score: number; index: number }
 
 export default defineComponent({
   components: {
     Hexagram,
-    CoinsInput,
   },
   setup() {
     const user = useUserStore()
     const name = ref(user.savedName)
+    const draw = ref(null)
+    const drawNumbersArray = computed(() => [...`${draw.value}`].map(Number))
     const { lines, addLine, hexagrams, trigrams } = useHexagrams()
-    const updateLines = ({ score, index }: { score: number; index: number }) => {
+    const updateLines = ({ score, index }: Args) => {
       addLine(score, index)
     }
 
+    watch(
+      () => drawNumbersArray.value,
+      () => {
+        if (isAllValidNumbers.value) {
+          const lastIndex = drawNumbersArray.value.length - 1
+          addLine(drawNumbersArray.value[lastIndex], lastIndex)
+        }
+      }
+    )
+
+    const isValid = computed(() => isFullLength.value && isAllValidNumbers.value)
+    const isFullLength = computed(() => drawNumbersArray.value.length === 6)
+    const isAllValidNumbers = computed(() => drawNumbersArray.value.every((n) => n >= 6 && n <= 9))
     const router = useRouter()
     const go = () => {
       if (name.value) router.push(`/hi/${encodeURIComponent(name.value)}`)
     }
 
     const { t } = useI18n()
-    return { t, name, go, lines, addLine, updateLines, hexagrams, trigrams }
+    return {
+      t,
+      name,
+      go,
+      lines,
+      addLine,
+      updateLines,
+      hexagrams,
+      trigrams,
+      draw,
+      isValid,
+      isFullLength,
+      isAllValidNumbers,
+    }
   },
 })
 </script>
@@ -43,12 +71,23 @@ export default defineComponent({
 
     <div class="flex flex-col">
       <div class="my-10 w-30 h-30 flex mx-auto">
-        <Hexagram :lines="lines"  />
+        <Hexagram :lines="lines" />
       </div>
 
       <div class="md:ml-6">
         <div class="w-full flex flex-col md:flex-row justify-center items-center md:items-start p-6">
-          <CoinsInput v-for="(line, i) in lines" :key="i" :line="line" :index="i" @onChange="updateLines" />
+          <div class="w-full md:w-1/2 md:mx-auto">
+            <input
+              v-model="draw"
+              class="border-2 border-primary transition h-12 px-5 pr-16 rounded-md focus:outline-none w-full text-black text-lg"
+              :class="{
+                'border-red-500': (draw && !isAllValidNumbers) || (isFullLength && !isValid),
+                'border-green-500': isValid,
+              }"
+              type="number"
+              name="search"
+              :placeholder="t('coin.input.placeholder')" />
+          </div>
         </div>
         <div v-if="hexagrams.situation" class="flex flex-col justify-center items-center py-6">
           <p class="text-xs dark:text-gray-200 text-center mb-2">Hexagramme de situation :</p>
@@ -85,3 +124,11 @@ export default defineComponent({
 meta:
   layout: home
 </route>
+
+<style scoped>
+input[type='number']::-webkit-inner-spin-button,
+input[type='number']::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+</style>
