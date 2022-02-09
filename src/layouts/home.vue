608ddf297eb095caef5ec990336b552b206cb24c
PierreTsia <pierre.tsiakkaros@gmail.com>
2022-02-09 23:50:30 +0100

    refact(layout): :iphone: adapt max-w for xl screens

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200 h-screen flex flex-col">
-    <div class="flex-grow lg:max-w-2/3 lg:min-w-1/3 xl:mx-auto">
+    <div class="flex-grow lg:max-w-2/3 xl:max-w-2/5 lg:mx-auto">
       <router-view />
     </div>
     <div>
